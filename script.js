eb68c2b339fa37755e8d74d6d271aadfde017b2b
Shruti <111372744+shru-6@users.noreply.github.com>
2023-09-09 00:16:13 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+//hero section
+document.addEventListener("DOMContentLoaded", function() {
+  const hero = document.querySelector(".hero");
+
+  setTimeout(() => {
+      hero.classList.add("active");
+  }, 200);
+});
+
+//input language card
 const dropdowns = document.querySelectorAll(".dropdown-container");
 const inputLanguageDropdown = document.querySelector("#input-language");
 const outputLanguageDropdown = document.querySelector("#output-language");
@@ -5,41 +15,63 @@ const outputLanguageDropdown = document.querySelector("#output-language");
 function populateDropdown(dropdown, options) {
   dropdown.querySelector("ul").innerHTML = "";
   options.forEach((option) => {
-    const li = document.createElement("li");
-    const title = option.name + " (" + option.native + ")";
-    li.innerHTML = title;
-    li.dataset.value = option.code;
-    li.classList.add("option");
-    dropdown.querySelector("ul").appendChild(li);
+      const li = document.createElement("li");
+      const title = option.name + " (" + option.native + ")";
+      li.innerHTML = title;
+      li.dataset.value = option.code;
+      li.classList.add("option");
+      dropdown.querySelector("ul").appendChild(li);
   });
 }
 
 populateDropdown(inputLanguageDropdown, languages);
 populateDropdown(outputLanguageDropdown, languages);
 
+
+//output language card
+const dropdownss = document.querySelectorAll(".dropdownn-container");
+const inputLanguageDropdownn = document.querySelector("#inputt-language");
+const outputLanguageDropdownn = document.querySelector("#outputt-language");
+
+function populateDropdown(dropdownn, options) {
+  dropdownn.querySelector("ul").innerHTML = "";
+  options.forEach((option) => {
+      const li = document.createElement("li");
+      const title = option.name + " (" + option.native + ")";
+      li.innerHTML = title;
+      li.dataset.value = option.code;
+      li.classList.add("option");
+      dropdownn.querySelector("ul").appendChild(li);
+  });
+}
+
+populateDropdown(inputLanguageDropdownn, languages);
+populateDropdown(outputLanguageDropdownn, languages);
+
+
+
 dropdowns.forEach((dropdown) => {
   dropdown.addEventListener("click", (e) => {
-    dropdown.classList.toggle("active");
+      dropdown.classList.toggle("active");
   });
 
   dropdown.querySelectorAll(".option").forEach((item) => {
-    item.addEventListener("click", (e) => {
-      dropdown.querySelectorAll(".option").forEach((item) => {
-        item.classList.remove("active");
+      item.addEventListener("click", (e) => {
+          dropdown.querySelectorAll(".option").forEach((item) => {
+              item.classList.remove("active");
+          });
+          item.classList.add("active");
+          const selected = dropdown.querySelector(".selected");
+          selected.innerHTML = item.innerHTML;
+          selected.dataset.value = item.dataset.value;
       });
-      item.classList.add("active");
-      const selected = dropdown.querySelector(".selected");
-      selected.innerHTML = item.innerHTML;
-      selected.dataset.value = item.dataset.value;
-    });
   });
 });
-
 document.addEventListener("click", (e) => {
   dropdowns.forEach((dropdown) => {
-    if (!dropdown.contains(e.target)) {
-      dropdown.classList.remove("active");
-    }
+      if (!dropdown.contains(e.target)) {
+          dropdown.classList.remove("active");
+      }
   });
 });
 
@@ -68,48 +100,217 @@ const uploadDocument = document.querySelector("#upload-document"),
 
 uploadDocument.addEventListener("change", (e) => {
   const file = e.target.files[0];
-  if (
-    file.type === "application/pdf" ||
-    file.type === "text/plain" ||
-    file.type === "application/msword" ||
-    file.type ===
+  if (file.type === "application/pdf") {
+      uploadTitle.innerHTML = file.name;
+
+      // Use pdf.js to extract text from the PDF file
+      const reader = new FileReader();
+      reader.readAsArrayBuffer(file);
+      reader.onload = async (e) => {
+          const arrayBuffer = e.target.result;
+          const pdf = await pdfjsLib.getDocument({
+              data: arrayBuffer
+          }).promise;
+          const text = [];
+          for (let i = 1; i <= pdf.numPages; i++) {
+              const page = await pdf.getPage(i);
+              const pageText = await page.getTextContent();
+              pageText.items.forEach((item) => {
+                  text.push(item.str);
+              });
+          }
+          inputTextElem.value = text.join("\n");
+      };
+  } else if (
+      file.type === "text/plain" ||
+      file.type === "application/msword" ||
+      file.type ===
       "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
   ) {
-    uploadTitle.innerHTML = file.name;
-    const reader = new FileReader();
-    reader.readAsText(file);
-    reader.onload = (e) => {
-      inputTextElem.value = e.target.result;
-    };
+      // Handle other supported file types
+      uploadTitle.innerHTML = file.name;
+      const reader = new FileReader();
+      reader.readAsText(file);
+      reader.onload = (e) => {
+          inputTextElem.value = e.target.result;
+      };
   } else {
-    alert("Please upload a valid file");
+      alert("Please upload a valid file");
   }
 });
+
+const startRecordButton = document.getElementById('startRecord');
+const stopRecordButton = document.getElementById('stopRecord');
+const audioPlayer = document.getElementById('audioPlayer');
+let mediaRecorder;
+let audioChunks = [];
+
+async function startRecording() {
+  const stream = await navigator.mediaDevices.getUserMedia({
+      audio: true
+  });
+  mediaRecorder = new MediaRecorder(stream);
+
+  mediaRecorder.ondataavailable = event => {
+      if (event.data.size > 0) {
+          audioChunks.push(event.data);
+      }
+  };
+
+  mediaRecorder.onstop = () => {
+      const audioBlob = new Blob(audioChunks, {
+          type: 'audio/wav'
+      });
+      audioPlayer.src = URL.createObjectURL(audioBlob);
+  };
+
+  mediaRecorder.start();
+  startRecordButton.disabled = true;
+  stopRecordButton.disabled = false;
+}
+
+function stopRecording() {
+  mediaRecorder.stop();
+  startRecordButton.disabled = false;
+  stopRecordButton.disabled = true;
+}
+
+startRecordButton.addEventListener('click', startRecording);
+stopRecordButton.addEventListener('click', stopRecording);
+
+// image
+const uploadImageInput = document.querySelector("#upload-image");
+const imagePreview = document.querySelector("#image-preview");
+const uploadImageTitle = document.querySelector("#upload-image-title");
+
+uploadImageInput.addEventListener("change", (e) => {
+  const file = e.target.files[0];
+  if (file) {
+      if (file.type.startsWith("image/")) {
+          uploadImageTitle.innerHTML = file.name;
+
+          // Display the image preview
+          const reader = new FileReader();
+          reader.onload = function(e) {
+              imagePreview.src = e.target.result;
+              imagePreview.style.display = "block";
+          };
+          reader.readAsDataURL(file);
+      } else {
+          alert("Please upload a valid image file.");
+      }
+  } else {
+      // Handle no file selected
+      alert("Please select an image to upload.");
+  }
+});
+
 
 const downloadBtn = document.querySelector("#download-btn");
 
 downloadBtn.addEventListener("click", (e) => {
   const outputText = outputTextElem.value;
   const outputLanguage =
-    outputLanguageDropdown.querySelector(".selected").dataset.value;
+      outputLanguageDropdown.querySelector(".selected").dataset.value;
   if (outputText) {
-    const blob = new Blob([outputText], { type: "text/plain" });
-    const url = URL.createObjectURL(blob);
-    const a = document.createElement("a");
-    a.download = `translated-to-${outputLanguage}.txt`;
-    a.href = url;
-    a.click();
+      const blob = new Blob([outputText], {
+          type: "text/plain"
+      });
+      const url = URL.createObjectURL(blob);
+      const a = document.createElement("a");
+      a.download = `translated-to-${outputLanguage}.txt`;
+      a.href = url;
+      a.click();
   }
 });
 
+//theme
 const darkModeCheckbox = document.getElementById("dark-mode-btn");
 
 darkModeCheckbox.addEventListener("change", () => {
   document.body.classList.toggle("dark");
 });
+// function handleThemeChange() {
+//   const themeCheckbox = document.getElementById('dark-mode-btn');
+//   const selectedTheme = themeCheckbox.checked ? 'light' : 'dark';
+//   localStorage.setItem('theme', selectedTheme);
+//   const body = document.body;
+//   if (selectedTheme === 'dark') {
+//     body.classList.add('dark');
+//   } else {
+//     body.classList.remove('dark');
+//   }
+// }
+
 
 const inputChars = document.querySelector("#input-chars");
 
 inputTextElem.addEventListener("input", (e) => {
   inputChars.innerHTML = inputTextElem.value.length;
+});
+
+
+
+//sign in pop up
+const signInButton = document.getElementById('signInButton');
+const signInPopup = document.getElementById('signInPopup');
+const closePopup = document.getElementById('closePopup');
+
+signInButton.addEventListener('click', () => {
+  signInPopup.style.display = 'block';
+});
+
+closePopup.addEventListener('click', () => {
+  signInPopup.style.display = 'none';
+});
+
+// Close the popup if the user clicks outside of it
+window.addEventListener('click', (event) => {
+  if (event.target === signInPopup) {
+      signInPopup.style.display = 'none';
+  }
+});
+
+
+
+//faq pop up
+const faqLink = document.getElementById('faqLink');
+const faqPopup = document.getElementById('faqPopup');
+const faqclosePopup = document.getElementById('faqclosePopup');
+
+faqLink.addEventListener('click', (event) => {
+  faqPopup.style.display = 'block';
+});
+
+faqclosePopup.addEventListener('click', () => {
+  faqPopup.style.display = 'none';
+});
+
+// Close the popup if the user clicks outside of it
+window.addEventListener('click', (event) => {
+  if (event.target === faqPopup) {
+      faqPopup.style.display = 'none';
+  }
+});
+
+
+
+//privacy policy pop up
+const ppLink = document.getElementById('ppLink');
+const ppPopup = document.getElementById('ppPopup');
+const ppclosePopup = document.getElementById('ppclosePopup');
+
+ppLink.addEventListener('click', (event) => {
+  ppPopup.style.display = 'block';
+});
+
+ppclosePopup.addEventListener('click', () => {
+  ppPopup.style.display = 'none';
+});
+
+// Close the popup if the user clicks outside of it
+window.addEventListener('click', (event) => {
+  if (event.target === ppPopup) {
+      ppPopup.style.display = 'none';
+  }
 });
